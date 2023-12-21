60d65ecdf53f454e8f8a1a25b7276f9286f17658
Francisco <73718340+Franciscopereira2001@users.noreply.github.com>
2023-12-21 10:32:16 +0000

    docker container

--- neo4jdocker.bat
+++ neo4jdocker.bat
@@ -1,1 +1,2 @@
- docker run --name testneo4j -p7474:7474 -p7687:7687 -d --env NEO4J_AUTH=neo4j/password neo4j:latest
+docker rm --force testneo4j
+docker run --name testneo4j -p7474:7474 -p7687:7687 -d --env NEO4J_AUTH=neo4j/password neo4j:latest
