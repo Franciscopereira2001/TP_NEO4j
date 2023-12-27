af3011e23881a9a625607c602b467d26a8dee82b
Francisco <73718340+Franciscopereira2001@users.noreply.github.com>
2023-12-27 07:08:07 +0000

    feito

--- gen/02_create.py
+++ gen/02_create.py
@@ -1,6 +1,5 @@
 from importlib import reload
-import sys
-import random
+import sys, random, datetime
 from carrega_cidades import *
 
 def gen_elemento(prefix, n):
@@ -8,6 +7,16 @@ def gen_elemento(prefix, n):
     for i in range(n):
         r.append(prefix + str(i))
     return r
+
+def gen_data():
+    start_date = datetime.datetime(2022, 10, 1, 1, 1, 1)
+    end_date   = datetime.datetime(2030, 11, 30, 1, 1, 1)
+    num_days   = (end_date - start_date).days
+    rand_days   = random.randint(1, num_days)
+    rand_hours  = random.randint(1, 24)
+    rand_min  = random.randint(1, 60)
+    random_date = start_date + datetime.timedelta(days=rand_days, hours=rand_hours, minutes=rand_min)
+    return random_date.strftime("20%y-%m-%dT%H:%M:%S")
 
 def main():
     n = 500
@@ -17,6 +26,7 @@ def main():
     enderecos = gen_elemento("addr_", n)
     users = gen_elemento("user_",n)
     parks = gen_elemento("park_",n)
+    orders = gen_elemento("order_", n*4)
     roles = ["Administrator", "Utilizador", "Convidado"]
     paises = ["Portugal"]
 
@@ -51,14 +61,48 @@ def main():
         print(f'CREATE ({user})-[:ROLE_IN]->({role})')
         
     # Parks
+    enderecos_tmp = enderecos
+    saved_parks = []
     for park in parks:
         lat = random.choices(range(n*1000))[0]
         long = random.choices(range(n*1000))[0]
-        print(f'{park}:Parkings {{name:"{park}",latitude:{lat}, longitude:{long}, enabled:true}}')
-        addr = random.choices(enderecos)[0]
-        enderecos.remove(addr)
-        print(len(enderecos))
-        print(f'CREATE ({park})-[:LOCATED_IN]->({addr}),')
+        print(f'CREATE({park}:Parkings {{name:"{park}",latitude:{lat}, longitude:{long}, enabled:true}})')
+        addr = random.choices(enderecos_tmp)[0]
+        enderecos_tmp.remove(addr)
+        print(f'CREATE ({park})-[:LOCATED_IN]->({addr})')
+        print(f'CREATE({park}_occupation:Occupation {{name:"{park}_o",percentage: {random.choices(range(100))}, last_updated_min:0}})')
+        print(f'CREATE ({park})-[:OCCUPATION_IN]->({park}_occupation)')
+        saved_parks.append({
+            "park": park,
+            "addr": addr
+        })
+    
+    order_per_user = {}
+    for order in orders:
+        print(f'CREATE({order}:Order {{name:"{order}",time:timestamp(), arrival:datetime("{gen_data()}")}})')
+        saved_park = random.choices(saved_parks)[0]
+        print(f'CREATE ({order})-[:SEARCH_IN]->({saved_park["addr"]})')
+        user = random.choices(users)[0]
+        print(f'CREATE ({user})-[:RESERVE_IN]->({order})')
+        print(f'CREATE ({order})-[:RESERVE_IN]->({saved_park["park"]})')
+        print(f'CREATE(Response_{order}:Response {{name:"Response_{order}", time:timestamp(), response:{random.choice(["true", "false"])}}})')
+        print(f'CREATE (Response_{order})-[:RESPONSE_IN]->({order})')
+        print(f'Create(Review_{order}:Review {{name:"Review_{order}", positiva: {random.choice(["true", "false"])}, descricao: "ok"}})')
+        print(f'CREATE (Review_{order})-[:REVIEW_IN]->({order})')
+        if user in order_per_user:
+            order_per_user[user].append(order)
+        else:
+            order_per_user[user] = [order]
+    
+    last = None
+    for _, orders in order_per_user.items():
+        for order in orders:
+            if last != None:
+                print(f'CREATE ({last})-[:PREVIOUS_IN]->({order})')
+            last = order
+                
+            
+        
         
 if __name__ == "__main__":
     main()
